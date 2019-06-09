684b52ff05ef40e545565c586361f8cf5bc21504
cakiran <cakiran@gmail.com>
2019-06-09 18:05:41 -0400

    Cleaned up commented code

--- RealReview/RealReview/Views/RealReviewFrameView.xaml.cs
+++ RealReview/RealReview/Views/RealReviewFrameView.xaml.cs
@@ -29,31 +29,5 @@ namespace RealReview
             BusinessReviewsViewModel businessReviewsViewModel = new BusinessReviewsViewModel();
             this.DataContext = businessReviewsViewModel;
         }
-
-        //private async void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    //var client = new HttpClient();
-        //    //// var address = new Uri("https://www.googleapis.com/customsearch/v1?q=restaurants+near+me&cx=012572433248785697579%3A1mazi7ctlvm&num=10&fields=items(link%2Cpagemap%2Ctitle)&key=AIzaSyCGXXa_kgjOiLa-NGsk4kgEbLZDEv8dJMQ" );
-
-        //    ////var addressnew = new Uri("https://www.googleapis.com/customsearch/v1?key=AIzaSyCGXXa_kgjOiLa-NGsk4kgEbLZDEv8dJMQ&cx=017576662512468239146:omuauf_lfve&q=restaurants+near+me");
-        //    //var address = new Uri("https://maps.googleapis.com/maps/api/place/textsearch/json?query=passagetoindia&sensor=true&key=AIzaSyCGXXa_kgjOiLa-NGsk4kgEbLZDEv8dJMQ");
-        //    //HttpResponseMessage response = await client.GetAsync(address);
-        //    //String stream = await response.Content.ReadAsStringAsync();
-
-        //    var client = new Yelp.Api.Client("2R3rYYTTNXbGEp5MJFXrV9JgRZkoDhZeCKcAQCgDyKzHnVKfN85E8nFq7AhibN2zr0vdAb7AvtCGRg2U2WrqCtigLg26xo4XxgEKPhjxDBxVLIdclwTpPdaX3kX8XHYx");
-        //    Yelp.Api.Models.SearchRequest searchRequest = new Yelp.Api.Models.SearchRequest();
-        //    searchRequest.Location = "28213";
-        //    searchRequest.Term = "Indian food";
-        //    var results = await client.SearchBusinessesAllAsync( searchRequest);
-        //    Yelp.Api.Models.SearchResponse searchResponse = results;
-        //    var businesses = searchResponse.Businesses;
-        //    foreach(var b in businesses)
-        //    {
-        //        var reviewresult = await client.GetReviewsAsync(b.Id);
-        //                                                                var reviews = reviewresult.Reviews;
-        //    }
-           
-
-        //}
     }
 }
